de5d624c6e83308a18f6e5af9c1e20806e185c81
juulioe <julio.eze.1999@gmail.com>
2021-10-07 20:49:57 -0400

    Login y register

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.get("/home",(req,res)=>{
      res.sendFile(path.join(__dirname,"./views/home1.html"))
  })
 
- app.get("/register",(req,res)=>{
+app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/register.html"))
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
 })
